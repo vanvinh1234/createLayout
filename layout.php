d170456dd62342525eb90ee5fd0b34c23c637c43
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-10-18 11:14:47 +0700

    layout with css

---
+++ layout.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div id="main">
+    <div id="head">header</div>
+    <div id="head-link">
+        <a href="#home">Home</a>
+        <a href="#contact">Contact</a>
+        <a href="#about us">About Us</a>
+    </div>
+    <div id="left">Left</div>
+    <div id="content">Content</div>
+    <div id="right">Right</div>
+    <div id="footer">Footer</div>
+</div>
+</body>
+</html>
